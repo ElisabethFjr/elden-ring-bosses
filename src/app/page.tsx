dc1f1e8873ec73214ad7e5bb6ebd225601de0b77
Elisabeth FAUJOUR <efaujour@gmail.com>
2023-11-15 18:06:13 +0100

    :feat: Btn Home Page Style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,8 @@ export default function Home() {
             lden Rin
             <span className="home-title-maj">g</span>
           </h1>
-          <h2 className="home-subtitle">Journey through the Lands Between and discover all bosses from the renowned FromSoftware's video game.</h2>
+          <h2 className="home-subtitle">Journey through the Lands Between and discover all bosses from the renowned FromSoftware&apos;s video game.</h2>
+          <button className="home-btn" type="button">All Bosses</button>
         </div>
       </section>
       <section className="bosses">
