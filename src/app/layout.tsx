4d97d48a3828494356f75441ff34beae516cf87a
Elisabeth FAUJOUR <efaujour@gmail.com>
2023-11-30 13:54:35 +0100

    :build: Config Axios & Eslint

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,18 @@
-import React from 'react';
-import type { Metadata } from 'next';
+import React from "react";
+import type { Metadata } from "next";
 
-import '../styles/globals.scss';
+import "../styles/globals.scss";
 
 export const metadata: Metadata = {
-  title: 'Elden Ring Bosses',
-  description: 'All bosses from the Elden Ring Video Game.',
+  title: "Elden Ring Bosses",
+  description: "All bosses from the Elden Ring Video Game. NextJs Application.",
+  keywords: "Elden Ring, From Software, Boss, Bosses, NextJs, React",
 };
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
