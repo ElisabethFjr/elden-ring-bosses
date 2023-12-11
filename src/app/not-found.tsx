31210b2549ebd2aa2f51a8dd8476453db4bcbb4d
Elisabeth FAUJOUR <efaujour@gmail.com>
2023-12-11 15:45:59 +0100

    :feat: BackButton Component

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,13 +1,11 @@
-import Link from 'next/link';
+import BackButton from '@/components/BackButton/BackButton';
 
 function NotFound() {
   return (
     <div className="error">
       <h1 className="error-title">Error 404</h1>
       <p className="error-text">Whooops, there is nothing here.</p>
-      <Link className="error-back-btn" href="/">
-        Back Home
-      </Link>
+      <BackButton />
     </div>
   );
 }
